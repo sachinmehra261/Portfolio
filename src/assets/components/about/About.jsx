c56b52719f8e9a2644162ff28e654e743dc9227b
sachin mehra <mehrasachin261@gmail.com>
2026-06-07 12:53:51 +0530

    v2 design fixed update

--- src/assets/components/about/About.jsx
+++ src/assets/components/about/About.jsx
@@ -1,50 +1,50 @@
 import React from "react";
 import styles from "./about.module.css";
-import profile from '../../images/profile.png'
-import fb from '../../images/fb.svg'
-import insta from '../../images/instagram.svg'
-import linkedin from '../../images/linkedin.svg'
+import profile from "../../images/profile.png";
 import { IoMdMail } from "react-icons/io";
 import { FaWhatsapp } from "react-icons/fa";
 
 function About() {
   return (
-    <>
-      <div id={styles.aboutMainContainer}>
-        <div id={styles.aboutContentContainer}>
-          <p>
-            Hello,<span>I'm</span>
-          </p>
-          <p>Sachin Mehra</p>
-          <p>Full Stack Developer</p>
-          <p>
-            I am committed to finding innovative solutions and continuously
-            experimenting to help my clients achieve their goals.
-          </p>
-          {/* FIXED: The href link quote is now perfectly closed */}
+    <div id={styles.aboutMainContainer}>
+      <div id={styles.aboutContentContainer}>
+        <p>
+          Hello, <span>I'm</span>
+        </p>
+        <p>Sachin Mehra</p>
+        <p>Full Stack Developer</p>
+        <p>
+          I am committed to finding innovative solutions and continuously
+          experimenting to help my clients achieve their goals.
+        </p>
+
+        <div className={styles.buttonContainer}>
+          {/* WhatsApp Button */}
           <a href="whatsapp://send?phone=9682348051&text=Hello%20Sachin%20Mehra">
             <button id={styles.whatsAppConnect}>
-              <p>Let’s Talk </p>
-              <FaWhatsapp id={styles.whatsAppLogo}/>
+              Let’s Talk <FaWhatsapp />
             </button>
           </a>
-          
-          <div id={styles.socialMedia}>
-            <span className={styles.socialMediaLabel}>Check out My</span>
-            <a href="https://www.facebook.com/profile.php?id=100015125777257" target="_blank" rel="noopener noreferrer"><img src={fb} alt="" /></a>
-            <a href="https://www.instagram.com/mehrasachin907/" target="_blank" rel="noopener noreferrer"><img src={insta} alt="" /></a>
-            <a href="https://www.linkedin.com/in/sachin-mehra-8a7541275" target="_blank" rel="noopener noreferrer"><img src={linkedin} alt="" /></a>
-            <a href="mailto:mehrasachin261@gmail.com" target="_blank" rel="noopener noreferrer"><IoMdMail id={styles.mailLogo}/></a>
-          </div>
-        </div>
 
-        <div id={styles.profilePicture}>
-          <div>
-            <img src={profile} alt="Sachin Mehra" className={styles.innerProfileImage} />
-          </div>
+          {/* Mail Button */}
+          <a href="mailto:mehrasachin261@gmail.com">
+            <button className={styles.mailConnect}>
+              Mail Me <IoMdMail />
+            </button>
+          </a>
         </div>
       </div>
-    </>
+
+      <div id={styles.profilePicture}>
+        <div>
+          <img
+            src={profile}
+            alt="Sachin Mehra"
+            className={styles.innerProfileImage}
+          />
+        </div>
+      </div>
+    </div>
   );
 }
 
